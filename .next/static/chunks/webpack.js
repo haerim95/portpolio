530f2640fe86316096b1b73ca7bef6057ae7e1eb
Hae Rim Kim <63902948+haerim95@users.noreply.github.com>
2021-10-17 03:07:31 +0900

    mail 보내기 작업

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4241029439f5c27236ac";
+/******/ 	var hotCurrentHash = "bbcf734a7b72e1d3db94";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/ContactMe.js
+++ components/ContactMe.js
@@ -1,77 +1,167 @@
-import React, { useEffect } from 'react';
-import { Form, Input, Button } from 'antd';
+import React, { useCallback, useEffect } from 'react';
+import useInput from '../hooks/useInput';
 import StyledTitle from './Titles';
 import { init } from 'emailjs-com';
-import { send } from 'emailjs-com';
+import emailjs from 'emailjs-com';
+import styled from 'styled-components';
+import { Row, Col } from 'antd';
 
-const onSubmit = (e) => {
-  send('ajmhoho', 'template_sv21t8n');
-};
-
-const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
+const Form = styled.form`
+  background-color: #5e4462;
+  padding: 2rem 4rem;
+  div {
+    + div {
+      margin-top: 1rem;
+    }
+  }
+  label {
+    line-height: 24px;
+    display: inline-block;
+    text-align: right;
+    font-size: 14px !important;
+    color: #eeeeee;
+  }
+  input {
+    width: 100%;
+    padding-left: 10px;
+    padding-bottom: 5px;
+    border: none;
+    border-bottom: 1px solid #eeeeee;
+    background-color: transparent;
+    outline: none;
+    color: #e7d8ee;
+    ::placeholder {
+      color: #e7d8ee;
+    }
+  }
+  textarea {
+    width: 100%;
+    height: calc(100% - 24px);
+    padding: 10px;
+    vertical-align: top;
+    border: 1px solid #eeeeee;
+    background-color: transparent;
+    outline: none;
+    resize: none;
+    color: #e7d8ee;
+    ::placeholder {
+      color: #e7d8ee;
+    }
+  }
+  .btnWrap {
+    text-align: right;
+    button {
+      width: 100px;
+      height: 40px;
+      border-radius: 5px;
+      background-color: #e7d8ee;
+      border: 1px solid #e7d8ee;
+      color: #5e4462;
+    }
+  }
+`;
 
 const ContactMe = () => {
+  // emailjs 초기 세팅
   useEffect(() => {
     init('user_O303wSkf7tVofLIJptu7o');
   }, []);
+  const [name, setName] = useInput('');
+  const [email, setEmail] = useInput('');
+  const [website, setWebsite] = useInput('');
+  const [introduction, setIntroduction] = useInput('');
+
+  const onSubmit = useCallback((e) => {
+    const inputNum = e.target.childElementCount - 1;
+    const data = new FormData(e.target);
+    const entries = data.entries();
+    let failNum = 0;
+
+    for (let i = 0; i < inputNum; i++) {
+      const next = entries.next();
+      const key = next.value[0];
+      const value = next.value[1];
+
+      if (!value) {
+        failNum++;
+        alert(`${key} 을 입력해주세요.`);
+        break;
+      }
+    }
+
+    if (!failNum) {
+      emailjs
+        .sendForm(
+          'service_d84t3za',
+          'template_sv21t8n',
+          e.target,
+          'user_O303wSkf7tVofLIJptu7o'
+        )
+        .then(
+          (result) => {
+            console.log('result.text', result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    }
+  }, []);
 
   return (
-    <div style={{ marginBottom: '4rem' }}>
+    <div>
       <StyledTitle>
         <span>💌 Contact Me </span>
       </StyledTitle>
-      <div style={{ width: '80%' }}>
-        <Form
-          {...layout}
-          name='messages'
-          // onFinish={onFinish}
-          // validateMessages={validateMessages}
-        >
-          <Form.Item
-            name={['user', 'name']}
-            label='성명'
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name={['user', 'email']}
-            label='Email'
-            rules={[
-              {
-                type: 'email',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item name={['user', 'website']} label='기업명'>
-            <Input />
-          </Form.Item>
-          <Form.Item name={['user', 'introduction']} label='Message'>
-            <Input.TextArea />
-          </Form.Item>
-          <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-            <Button
-              style={{ backgroundColor: '#5e4462', color: '#ffffff' }}
-              htmlType='submit'
-            >
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
+
+      <Form onSubmit={onSubmit}>
+        <Row>
+          <Col xs={24} md={12} style={{ paddingRight: '3rem' }}>
+            <div>
+              <label>성함</label>
+              <input
+                type='text'
+                name='name'
+                placeholder='성함을 입력해주세요'
+                value={name}
+                onChange={setName}
+              />
+            </div>
+            <div>
+              <label>기업명</label>
+              <input
+                type='text'
+                name='website'
+                placeholder='기업명을 입력해주세요'
+                value={website}
+                onChange={setWebsite}
+              />
+            </div>
+            <div>
+              <label>이메일</label>
+              <input
+                type='text'
+                name='email'
+                placeholder='이메일을 입력해주세요'
+                value={email}
+                onChange={setEmail}
+              />
+            </div>
+          </Col>
+          <Col xs={24} md={12}>
+            <label>메일 내용</label>
+            <textarea
+              name='introduction'
+              placeholder='메세지를 입력해주세요.'
+              value={introduction}
+              onChange={setIntroduction}
+            />
+          </Col>
+        </Row>
+        <div className='btnWrap'>
+          <button type='submit'> 발송 </button>
+        </div>
+      </Form>
     </div>
   );
 };
