abb6b72a1918b995e251558cac68e70d8d7f3238
Hae Rim Kim <63902948+haerim95@users.noreply.github.com>
2021-10-19 05:03:43 +0900

    Deploy Next.js to gh-pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const withImages = require('next-images');
-module.exports = withImages();
+// module.exports = withImages();
 
 const debug = process.env.NODE_ENV !== 'production';
 const name = 'small-magic-project-deployment';
 
 module.exports = {
+  withImages,
   assetPrefix: !debug ? `/${name}/` : '',
 };
